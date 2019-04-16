a4d883763956db0ed618acd4309f42ebe891a73f
istvanskeri <istvanskeri@gmail.com>
2019-04-16 11:38:59 +0200

    login test formatting

--- server/test/login.js
+++ server/test/login.js
@@ -1,22 +1,21 @@
 var app = require('../index.js'),
-  assert = require('assert'),
-  chai = require('chai'),
-  should = require('should');
-  request = require('supertest');
+	assert = require('assert'),
+	chai = require('chai'),
+	should = require('should'),
+	request = require('supertest');
 
 describe('POST /login', function() {
-  it('logs user in', function(done) {
-    request(app)
-      .post('/api/auth/login')
-      .send({email: 'istvanskeri@gmail.com', password: 'evensteven'})
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err,res){
-        response = res;
-        res.status.should.equal(200);
-        res.body.success.should.equal(true);
-        done();
-      });
-  });
+	it('logs user in', function(done) {
+		request(app)
+			.post('/api/auth/login')
+			.send({email: 'istvanskeri@gmail.com', password: 'evensteven'})
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end(function(err,res){
+				res.status.should.equal(200);
+				res.body.success.should.equal(true);
+				done();
+			});
+	});
 });
